9d4a19ab095c841ee2f34e763b027d86fd313cac
megglos <sebastian.bathke@googlemail.com>
2015-08-14 11:35:19 +0200

    removed eureka

--- src/main/groovy/com/nike/mm/AppConfig.groovy
+++ src/main/groovy/com/nike/mm/AppConfig.groovy
@@ -1,12 +1,10 @@
 package com.nike.mm
 
-import com.netflix.appinfo.AmazonInfo
 import org.dozer.DozerBeanMapper
 import org.dozer.Mapper
 import org.jasypt.util.text.BasicTextEncryptor
 import org.jasypt.util.text.TextEncryptor
 import org.springframework.beans.factory.annotation.Value
-import org.springframework.cloud.netflix.eureka.EurekaInstanceConfigBean
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories
@@ -51,10 +49,4 @@ class AppConfig {
         return scheduler;
     }
 
-    @Bean
-    EurekaInstanceConfigBean eurekaInstanceConfigBean() {
-        final EurekaInstanceConfigBean eurekaInstanceConfigBean = new EurekaInstanceConfigBean();
-        eurekaInstanceConfigBean.dataCenterInfo = AmazonInfo.Builder.newBuilder().autoBuild("eureka.");
-        return eurekaInstanceConfigBean
-    }
 }
