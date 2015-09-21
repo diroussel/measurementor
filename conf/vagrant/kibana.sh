eff17c48f36a051ec5d579b0d6dbcc5ade63ee43
megglos <sebastian.bathke@googlemail.com>
2015-09-21 19:47:43 +0200

    updated kibana script

--- conf/vagrant/kibana.sh
+++ conf/vagrant/kibana.sh
@@ -1,17 +1,13 @@
 #!/usr/bin/env bash
 
+mkdir /opt/kibana
 cd /home/vagrant
+sudo wget https://download.elastic.co/kibana/kibana/kibana-4.1.1-linux-x64.tar.gz
+tar -zxvf kibana-4.1.1-linux-x64.tar.gz
+sudo rm kibana-4.1.1-linux-x64.tar.gz
+cp /home/vagrant/kibana-*/* /opt/kibana/ -R
 
-mkdir kibana
-
-cd kibana
-
-sudo wget https://download.elastic.co/kibana/kibana/kibana-4.1.0-linux-x64.tar.gz
-
-tar -zxvf kibana-4.1.0-linux-x64.tar.gz
-
-sudo rm kibana-4.1.0-linux-x64.tar.gz
-
-cd kibana-4.1.0-linux-x64
-
-./bin/kibana &
+cd /etc/init.d && sudo wget https://gist.githubusercontent.com/thisismitch/8b15ac909aed214ad04a/raw/bce61d85643c2dcdfbc2728c55a41dab444dca20/kibana4
+chmod +x /etc/init.d/kibana4
+update-rc.d kibana4 defaults 96 9
+service kibana4 start
